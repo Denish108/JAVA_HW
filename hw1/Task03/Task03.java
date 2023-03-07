8d468bdd015545ae0672d2b7a44fcc87891d0410
Denish108 <teabagwithoutmeat@gmail.com>
2023-03-07 18:48:45 +0300

    fin

---
+++ hw1/Task03/Task03.java
@@ -0,0 +1,9 @@
+// Реализовать простой калькулятор Введите число 2 Введите операцию 1 + 2 - 3 * 4 / Введите число 2 2 + 2 = 4
+package Task03;
+
+public class Task03 {
+    public static void main(String[] args) {
+        Controller.start();
+    }
+
+}
