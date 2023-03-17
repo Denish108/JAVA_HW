47e90d4d8128a1bd977051333d21a0d1ee0b5e0e
Denish108 <teabagwithoutmeat@gmail.com>
2023-03-17 19:37:44 +0300

    hw5 done2

--- hw5/Task51.java
+++ hw5/Task51.java
@@ -1,20 +1,16 @@
 package hw5;
 
-
-
 import java.util.HashMap;
-
 import java.util.List;
 
 public class Task51 {
     public static void main(String[] args) {
         HashMap<String, List<String>> phoneBook = new HashMap<>();
 
-        phoneBook.put("Ivanov", List.of("+7954656", "555678"));
-        phoneBook.put("Petrov", List.of("+75654656", "+7908555678"));
-        phoneBook.put("Sidorov", List.of("+7910878", "883130"));
+        phoneBook.put("Ivanov", List.of("+79991222222", "+79882111111"));
+        phoneBook.put("Petrov", List.of("+799842333333", "+79883222222"));
+        phoneBook.put("Sidorov", List.of("+79893444444", "+31234350"));
         
         System.out.println(phoneBook);
     }
-
 }
